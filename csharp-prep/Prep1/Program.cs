1a626faa4b0ac654f487227dfff3437de4dccbfd
Luke Wilson <wil21061@byui.edu>
2023-09-13 09:55:39 -0600

    started using basic c# added name to readme

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        //string interpolation
+        //int myInt = 5;
+
+        //Console.WriteLine($"my int = {myInt}");
+
+        //readline
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        //Console.WriteLine($"name = {name}");
+
+        //conditionals
+        if (name == "Luke")
+        {
+            Console.WriteLine("Hi Luke");
+        }
+        else
+        {
+            Console.WriteLine($"Hi {name}");
+        }
+
+
     }
 }
