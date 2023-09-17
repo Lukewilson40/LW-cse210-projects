3e117c9db22c31a1abecd3182a42b11cf8fc8469
Luke Wilson <wil21061@byui.edu>
2023-09-16 21:24:47 -0600

    completed assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        char letterGrade;
+
+        if (gradePercentage >= 90)
+        {
+            letterGrade = 'A';
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = 'B';
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = 'C';
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = 'D';
+        }
+        else
+        {
+            letterGrade = 'F';
+        }
+
+        Console.WriteLine("Your letter grade is: " + letterGrade);
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass the course. Keep working hard for next time!");
+        }
     }
 }
