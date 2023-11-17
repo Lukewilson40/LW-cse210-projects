04926bcbd230be972e9936786c4449793f8309af
Luke Wilson <wil21061@byui.edu>
2023-11-17 16:10:17 -0700

    polymorphism and shapes stuff

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,81 @@
 using System;
+using System.Collections.Generic;
+
+abstract class Shape
+{
+    public string Color { get; set; }
+
+    public abstract double GetArea();
+}
+
+public class Square : Shape
+{
+    private double Side { get; set; }
+
+    public Square(string color, double side)
+    {
+        Color = color;
+        Side = side;
+    }
+
+    public override double GetArea()
+    {
+        return Side * Side;
+    }
+}
+
+class Rectangle : Shape
+{
+    private double Length { get; set; }
+    private double Width { get; set; }
+
+    public Rectangle(string color, double length, double width)
+    {
+        Color = color;
+        Length = length;
+        Width = width;
+    }
+
+    public override double GetArea()
+    {
+        return Length * Width;
+    }
+}
+
+class Circle : Shape
+{
+    private double Radius { get; set; }
+
+    public Circle(string color, double radius)
+    {
+        Color = color;
+        Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("Red", 3),
+            new Rectangle("Blue", 4, 5),
+            new Circle("Green", 6)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.Color;
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
+
